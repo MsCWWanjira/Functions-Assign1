ad39269939658f3cb805fafce7b50fab8a70784f
Wairimu_CWanjira <161702454+MsCWWanjira@users.noreply.github.com>
2024-05-15 17:51:45 +0300

    Update Question1.js

--- Question1.js
+++ Question1.js
@@ -27,3 +27,19 @@ let playerScore = 0;
         (playerChoice === 'paper' && computerChoice === 'rock') ||
         (playerChoice === 'scissors' && computerChoice === 'paper') ? 'Win' : 'Lose'
     );
+function displayResult(outcome, playerChoice, computerChoice) {
+        const resultDiv = document.getElementById('result');
+        resultDiv.innerHTML = `You chose ${playerChoice}. Computer chose ${computerChoice}. <br> You ${outcome}!`;
+
+        if (outcome === 'Win') {
+            playerScore++;
+        } else if (outcome === 'Lose') {
+            computerScore++;
+        }
+        resultDiv.innerHTML += `<br> Player Score: ${playerScore} | Computer Score: ${computerScore}`;
+
+        if (playerScore === 3 || computerScore === 3) {
+            const winner = playerScore === 3 ? 'You' : 'Computer';
+            resultDiv.innerHTML += `<br> **${winner} wins!**`;
+        }
+    }
